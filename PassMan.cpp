168c3f5d381c66b709ba1db3fd216a0c07a8bb97
Rishikant <rishikantsahu181@gmail.com>
2020-06-23 10:52:27 +0530

    Pass-man now sends alert mails regarding unsuccessful attempt to access passwords

--- PassMan.cpp
+++ PassMan.cpp
@@ -4,10 +4,12 @@
 #include <string>
 #include <fstream>
 #include <filesystem>
+#include <ctime>
 #include "Crypto.h"
 #include "Helper.h"
 #include "MangMethods.h"
 #include "SecMethods.h"
+#include "Mailman.h"
 
 // Global vars
 int enKeyMul = 0;                                       // To store necryption key multiplicative part
@@ -88,6 +90,31 @@ int main(int nArgs, char *allArgs[]) {
     // If authorization fails
     if (authStatus == false) {
         std::cout << "Failed to authorize. Suspending further operations" << std::endl;
+
+        // Alerting user if mail-id set up
+        if (std::filesystem::exists(mailIdLoc) == true) {
+            std::string mailId;                          // To store mail-id from file
+            std::string timeString;                      // To store time as string
+            time_t now = time(NULL);                     // To store time_t
+            char nowTime[26];                            // To store current time
+
+            // Getting mail-id from file
+            inpFile.open(mailIdLoc);
+            std::getline(inpFile, mailId);
+            inpFile.clear();
+            inpFile.close();
+
+            // Decrypting mail-id
+            mailId = inpDecrypt(mailId);
+
+            // Getting current time
+            ctime_s(nowTime, 26, &now);
+            // Storing time as string
+            timeString = nowTime;
+
+            // Sending mail
+            sendMail(mailId, "Unsuccessful access attempt of pass-man", "Someone tried to access your pass-man passwords at time : " + timeString);
+        }
         return 0;
     }
 
